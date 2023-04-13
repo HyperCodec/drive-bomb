b513dba6e3a6379d24b24d1b7972125a02e0d267
HyperCodec <72839119+HyperCodec@users.noreply.github.com>
2023-04-13 16:10:39 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 from threading import Thread
 import asyncio, os
-from multiprocessing import Pool
 
-def run():
-    async def chaos():
-        nonlocal suffering
-    
-        while True:
-            suffering += "𒁃"
-    
-    def save():
-        while True:
-            with open("pain.bin", "a") as f:
-                 f.write(suffering)
+# config (if someone wants to run this for some reason)
+output_dir = "foo.txt"
+async_tasks = 200
 
-            print('saved')
-            print(f'filesize (bytes): {os.path.getsize("pain.bin")}')
+# the fun part
+async def chaos():
+    global data
+    
+    while True:
+        data += "𒁃"
+    
+def save():
+    while True:
+        with open(output_dir, "a") as f:
+             f.write(data)
+
+        print('saved')
+        print(f'filesize (bytes): {os.path.getsize(output_dir)}')
             
-    suffering = ""
-    t = Thread(target=save)
-    t.start()
+data = ""
+t = Thread(target=save)
+t.start()
     
-    for i in range(200):
-        asyncio.run(chaos())
-
-with Pool(10) as p:
-    run()
+for i in range(async_tasks):
+    asyncio.run(chaos())
